92240c64ad36ab2fca63e8c3de89bff38ad8d403
Aaron Miller <amiller@aps.org>
2024-09-30 05:14:01 +0000

    initial commit

---
+++ jest.config.ts
@@ -0,0 +1,14 @@
+export default {
+  displayName: 'aps-candy-showdown',
+  preset: './jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/next/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: './coverage/aps-candy-showdown',
+  testMatch: [
+    '<rootDir>/src/**/__tests__/**/*.[jt]s?(x)',
+    '<rootDir>/src/**/*(*.)@(spec|test).[jt]s?(x)',
+  ],
+};
